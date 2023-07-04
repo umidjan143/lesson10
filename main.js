9ae8aa39f046c2d19c7ab14b30c2554c47e4148d
umidjan143 <xasanbayevumidjon@gmail.com>
2023-07-04 17:58:02 +0500

    homework lesson 10

---
+++ main.js
@@ -0,0 +1,428 @@
+//1
+
+// class students {
+//     constructor ( name, age, IELTS )
+//     {
+//         this.name = name ;
+//         this.age = age ;
+//         this.IELTS=IELTS ;
+//     }
+// }
+
+// const Ali = new students ('Ali' , 20 , 5) ;
+// const Umidjon = new students ('Umidjon' , 19,  6) ;
+// const Adxam = new students ('Adxam' , 18,  6) ;
+
+//2
+
+// class groups {
+//     constructor ( name, age, IELTS )
+//     {
+//         this.name = name ;
+//         this.members = members ;
+//         this.level=level ;
+//     }
+// }
+
+// const n8 = new groups ('React N8' , 20 , 'middle') ;
+// const n9 = new groups ('React N9' , 21,  'junior') ;
+// const n10 = new groups ('React N10' , 18,  'beginner') ;
+
+//3
+
+// class logistics_Company {
+//     constructor ( name, trucks, country )
+//     {
+//         this.name = name ;
+//         this.trucks = trucks;
+//         this.country=country ;
+//     }
+// }
+
+// const alz = new logistics_Company ('ALZ Express' , 115 , 'Uzbekistan ') ;
+// const bobstar= new logistics_Company ('Bobstar' , 900,  'the USA') ;
+// const masterload= new logistics_Company ('Masterload' , 550,  'New Zealand') ;
+
+//4
+
+// class parks {
+//     constructor ( name, city)
+//     {
+//         this.name = name ;
+//         this.city= city;
+//     }
+// }
+
+// const magic  = new parks ('Magic City' , 'Tashkent ') ;
+
+// const disneyland = new parks ('Disneyland , New york ') ;
+
+//5
+
+// class parks {
+//     constructor ( name, city)
+//     {
+//         this.name = name ;
+//         this.city= city;
+//     }
+// }
+
+// const magic  = new parks ('Magic City' , 'Tashkent ') ;
+
+// const disneyland = new parks ('Disneyland ' , 'New York') ;
+
+//6
+
+// class fastfood {
+//     constructor ( name, country)
+//     {
+//         this.name = name ;
+//         this.country= country;
+//     }
+// }
+
+// const KFC  = new fastfood ('Kentucky Fried Chicken' , 'The USA ') ;
+
+// const EVOS = new fastfood ('EVOS ' , 'Uzbekistan') ;
+
+//7
+
+// class Football_clubs {
+//     constructor ( name, UEFA_cups)
+//     {
+//         this.name = name ;
+//         this.UEFA_cups=UEFA_cups;
+//     }
+// }
+
+// const barsa = new Football_clubs ('FC.Barcelona' , 5 ) ;
+
+// const real = new Football_clubs ('Real Madrid ' , 13 ) ;
+
+//8
+
+// class Planets {
+//     constructor ( situation, moons )
+//      {
+//         this.situation=situation;
+//         this.moons=moons;
+//     }
+// }
+
+// const mars = new Planets ('4th' , 2 ) ;
+
+// const saturn = new Planets ('6th ' , 70 ) ;
+
+//9
+
+// class Games {
+//     constructor ( name, fanbase )
+//      {
+//         this.name=name;
+//         this.fanbase=fanbase;
+//     }
+// }
+
+// const pubg = new Games ('Player Unknowns Battlegrounds' , '900 million'  ) ;
+
+// const mlbb = new Games ('Mobile Legends Bang Bang ' , '1 billion' ) ;
+
+//10
+
+// class Games {
+//     constructor ( name, fanbase )
+//      {
+//         this.name=name;
+//         this.fanbase=fanbase;
+//     }
+// }
+
+// const pubg = new Games ('Player Unknowns Battlegrounds' , '900 million'  ) ;
+
+// const mlbb = new Games ('Mobile Legends Bang Bang ' , '1 billion' ) ;
+
+//11
+
+// class cars {
+//     constructor (  country, topSpeed )
+//      {
+//         this.country=country;
+//         this.topSpeed=topSpeed;
+
+//     }
+// }
+
+// const Koenigsegg = new cars ('Sweden' , '487km/h'  ) ;
+
+// const Bugatti = new cars ('France' , '410km/h' ) ;
+
+//12
+
+// class country {
+//     constructor ( capital , population )
+//      {
+//         this.capital=capital;
+//         this.population=population;
+
+//     }
+// }
+
+// const Uzbekistan = new country ('Tashkent' , '36 million'  ) ;
+
+// const USA = new country ('Washington D.C' , ' 450 million' ) ;
+
+//13
+
+// class apps {
+//     constructor ( users , known )
+//      {
+//         this.users=users;
+//         this.known=known;
+
+//     }
+// }
+
+// const Instagram = new apps ( 'more than half billion' , 'worldwide'  ) ;
+
+// const Telegram = new apps ('100 million' , ' worldwide' ) ;
+
+//14
+
+// class celebrity {
+//     constructor ( job , followers )
+//      {
+//         this.job=job;
+//         this.followers = followers;
+
+//     }
+// }
+
+// const Ronaldo = new celebrity ( 'football player ' , '594 million'  ) ;
+
+// const Messi = new celebrity ('football player' , ' 477 million' ) ;
+
+//15
+
+// class laptops {
+//     constructor ( name , cost )
+//      {
+//         this.name=name;
+//         this.cost=cost;
+
+//     }
+// }
+
+// const hp = new laptops ( 'HP ' , '$500'  ) ;
+
+// const asus = new laptops ('Asus' , '$500' ) ;
+
+//16
+
+// class Iphone {
+//     constructor ( name , cost )
+//      {
+//         this.name=name;
+//         this.cost=cost;
+
+//     }
+// }
+
+// const Iphone14 = new Iphone ( 'Iphone14 ' , '$1000'  ) ;
+
+// const Iphone13 = new Iphone ('Iphone14' , '$700' ) ;
+
+//17
+
+// class House {
+//     constructor (location , cost )
+//     {this.location=location;
+//     this.cost=cost}
+// }
+
+// const twoRoom = new House ( 'Chilonzor' , '$60000' ) ;
+// const threeRoom = new House ( 'Yunusobod' , '$45000' ) ;
+
+// Keyboard
+
+// class Keyboard {
+//   constructor(number, functions) {
+//     this.number = number;
+//     this.functions = functions;
+//   }
+// }
+
+// const esc = new Keyboard(1, "Quit all files, back to previous file");
+
+// const f1 = new Keyboard(2, "Opens the Help system for the active window");
+
+// const f2 = new Keyboard(3, "Light down");
+
+// const f3 = new Keyboard(4, "Light up");
+
+// const f4 = new Keyboard(5, "Creates a new document in programs that support this command");
+
+// const f5 = new Keyboard(6, "Refresh");
+
+// const f6 = new Keyboard(7, "No volume");
+
+// const f7 = new Keyboard(8, "Volume down");
+
+// const f8 = new Keyboard(9, "Volume Up");
+
+// const f9 = new Keyboard(10, "Play previous media file");
+
+// const f10 = new Keyboard(11, "play and pause media file");
+
+// const f11 = new Keyboard(12, "play next media file");
+
+// const f12 = new Keyboard(13, "Pilot mode");
+
+// const prt_sc = new Keyboard(14, "Make screenshot of screen");
+
+// const del = new Keyboard(15, "delete anything");
+
+// const home = new Keyboard(16, "auto go to beginning of row");
+
+// const end = new Keyboard(17, "auto go to end of row");
+
+// const pg_up = new Keyboard(18, "auto go to beginning of page");
+
+// const pg_down = new Keyboard(19, "auto go to end of page");
+
+// const Backtick = new Keyboard(20, "write symbol of ` or ~ ");
+
+// const num1 = new Keyboard(21, "Write 1 or ! ");
+
+// const num2 = new Keyboard(22, "write 2 or @ ");
+// const num3 = new Keyboard(23, "write 3 or # ");
+// const num4 = new Keyboard(24, "write 4 or $ ");
+// const num5 = new Keyboard(25, "write 5 or % ");
+// const num6 = new Keyboard(26, "write 6 or ^ ");
+// const num7 = new Keyboard(27, "write 7 or & ");
+// const num8 = new Keyboard(28, "write 8 or * ");
+// const num9 = new Keyboard(29, "write 9 or ( ");
+// const num0 = new Keyboard(30, "write 0 or ) ");
+// const minus = new Keyboard(31, "write - or _ ");
+// const equals = new Keyboard(32, "write = or + ");
+// const Backspace = new Keyboard(33, "The Backspace button is used to delete the character before the cursor or the selected item.");
+
+
+// const numLock = new Keyboard(34, "block the ability of writing numbers on num pad");
+// const slash = new Keyboard(35, "Write / ");
+// const Asterisk = new Keyboard(36, "Write * ");
+// const minus2 = new Keyboard(37, "Write - ");
+// const tab = new Keyboard(38, "Moves the cursor or focus to the next selectable element. ");
+
+// const q = new Keyboard(39, "Write q ");
+// const w = new Keyboard(40, "Write w ");
+// const e = new Keyboard(41, "Write e ");
+// const r = new Keyboard(42, "Write r ");
+// const t = new Keyboard(43, "Write t ");
+// const y = new Keyboard(44, "Write y ");
+// const u = new Keyboard(45, "Write u ");
+// const i = new Keyboard(46, "Write i ");
+// const o = new Keyboard(47, "Write o ");
+// const p = new Keyboard(48, "Write p ");
+// const leftBracket = new Keyboard(49, "Write { or [ ");
+// const rightBracket = new Keyboard(50, "Write } or ] ");
+// const backslash = new Keyboard(51, "Write \ or |  "); 
+
+// const numpad7 = new Keyboard(52, "Write 7 or function Home in num lock ");
+// const numpad8 = new Keyboard(53, "Write 8  ");
+// const numpad9 = new Keyboard(54, "Write 9 or function Page up in num lock ");
+// const capslock = new Keyboard(55, "Write all letters in capital ");
+// const a = new Keyboard(56, "Write a ");
+// const s = new Keyboard(57, "Write s ");
+// const d = new Keyboard(58, "Write d ");
+// const f = new Keyboard(59, "Write f ");
+// const g = new Keyboard(60, "Write g ");
+// const h = new Keyboard(61, "Write h ");
+// const j = new Keyboard(62, "Write j ");
+// const k = new Keyboard(63, "Write k ");
+// const l = new Keyboard(64, "Write l ");
+// const Semicolon = new Keyboard(65, "Write ; or : ");
+// const Apostrophe = new Keyboard(66, "Write ' or " );
+// const enter = new Keyboard(67, 'submit anything');
+
+
+// const numpad4 = new Keyboard(68, "Write 4  ");
+// const numpad5 = new Keyboard(69, "Write 5  ");
+// const numpad6 = new Keyboard(70, "Write 6 ");
+// const numpadplus = new Keyboard(71, "Write + ");
+// const leftshift = new Keyboard(72, "The Shift button is used to type capital letters, access symbols on the upper part of keys, or activate secondary functions of certain keys.");
+
+
+// const z = new Keyboard(73, "Write d ");
+// const x = new Keyboard(74, "Write f ");
+// const c = new Keyboard(75, "Write g ");
+// const v = new Keyboard(76, "Write h ");
+// const b = new Keyboard(77, "Write j ");
+// const n = new Keyboard(78, "Write k ");
+// const m = new Keyboard(79, "Write l ");
+// const comma = new Keyboard(80, "Write , or < ");
+// const period = new Keyboard(81, "Write . or > ");
+// const question  = new Keyboard(82, "Write ? or / ");
+// const rightshift = new Keyboard(83, "The Shift button is used to type capital letters, access symbols on the upper part of keys, or activate secondary functions of certain keys.");
+
+
+
+// const numpad1 = new Keyboard(84, "Write 1 or funtion end in num lock  ");
+// const numpad2 = new Keyboard(85, "Write 2");
+// const numpad3 = new Keyboard(86, "Write 3 or  funtion Page down in num lock ");
+
+
+
+// const leftctrl = new Keyboard(87, " The Ctrl (Control) button is used as a modifier key in combination with other keys to perform various shortcuts and commands within an application or operating system.");
+// const rightctrl = new Keyboard(88, " The Ctrl (Control) button is used as a modifier key in combination with other keys to perform various shortcuts and commands within an application or operating system.");
+// const fn = new Keyboard(89, " button is used to activate secondary functions or special features assigned to specific keys on a laptop keyboard. It allows you to access additional functions such as adjusting screen brightness, volume control, multimedia playback, and other specialized commands.");
+
+
+// const windows = new Keyboard(90, "button (Win) is used to access the Start menu or Start screen, open the taskbar, and navigate through the operating system on Windows-based laptops. It provides quick access to system features, search functionality, and application shortcuts.");
+
+
+
+// const leftalt = new Keyboard(91, "button is used as a modifier key in combination with other keys to access alternate characters, trigger shortcuts, or activate specific functions within an application.");
+
+
+// const space= new Keyboard(92, " put space between any objects ");
+
+
+// const rightalt = new Keyboard(93, "button is used as a modifier key in combination with other keys to access alternate characters, trigger shortcuts, or activate specific functions within an application.");
+
+// const left = new Keyboard ( 94 , 'move cursor to left')
+// const up= new Keyboard ( 95 , 'move cursor to up')
+// const down= new Keyboard ( 96 , 'move cursor to down')
+// const right = new Keyboard ( 97 , 'move cursor to right')
+
+
+
+
+
+// const numpad0 = new Keyboard(98, "Write 0 ");
+// const numpaddel = new Keyboard(99, "Write . or delete function in num lock ");
+// const numpadenter = new Keyboard(100, 'submit anything');
+// const power = new Keyboard(101, 'power on and off of laptop');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Person (name , age) {
+//     this.name=name ;
+//     this.age = age ;
+
+// }
+
+// const vali = new Person ( "vali" , 20) ;
+// const ali = new Person ( "ali" , 16) ;
